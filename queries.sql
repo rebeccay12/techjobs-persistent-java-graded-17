91427650752a5bd3d4b63669b59982a1228a4b57
rebeccay12 <beccayeager97@gmail.com>
2023-11-13 17:51:49 -0600

    task 3 and 4

--- queries.sql
+++ queries.sql
@@ -1,8 +1,13 @@
 --Part 1
-DROP TABLE job;
+
 
 --Part 2
 
 --Part 3
+DROP TABLE job;
 
 --Part 4
+SELECT skill.skill_name
+FROM skill
+JOIN job ON skill.skill_id = job.skill_id
+ORDER BY skill.skill_name;
